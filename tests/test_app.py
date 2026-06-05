b8994a4d2f5af9a11ef3ac9578098561f98ed07e
igiacc <igor.simonovic@accenture.com>
2026-06-05 12:59:40 +0000

    commit4

--- tests/test_app.py
+++ tests/test_app.py
@@ -18,7 +18,7 @@ client = TestClient(app)
 
 
 def test_root_redirects_to_static_index():
-    response = client.get("/", allow_redirects=False)
+    response = client.get("/", follow_redirects=False)
     assert response.status_code in (301, 302, 307, 308)
     assert response.headers["location"] == "/static/index.html"
 
